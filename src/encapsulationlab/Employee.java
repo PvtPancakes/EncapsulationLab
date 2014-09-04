642a1c9942ccb4addea8d5a649bef20b534c5e59
eennis <eennis@L11702042765.wctc.msft>
2014-09-04 16:37:20 -0500

    Made all properties and setters of Employee private and set up a createEmployee method to set firstName, lastName, and ssn.

---
+++ src/encapsulationlab/Employee.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package encapsulationlab;
+
+import java.util.Date;
+
+/**
+ *
+ * @author eennis
+ */
+public class Employee {
+    
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private Date birthDate;
+    private boolean metWithHr;
+    private boolean metDeptStaff;
+    private boolean reviewedDeptPolicies;
+    private boolean movedIn;
+    private String cubeId;
+
+    public Employee() {
+
+    }
+    
+    public Employee(String firstName, String lastName, String ssn){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ssn = ssn;
+    }
+    
+    public void createEmployee(String firstName, String lastName, String ssn){
+        setFirstName(firstName);
+        setLastName(lastName);
+        setSsn(ssn);
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        if(firstName.length() == 0 || firstName == null){
+            System.out.println("First Name not entered");
+            System.exit(0);
+        }
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void setLastName(String lastName) {
+        if(lastName.length() == 0 || lastName == null){
+            System.out.println("Last Name not entered");
+            System.exit(0);
+        }
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    private void setSsn(String ssn) {
+        if(ssn.length() == 0 || ssn == null){
+            System.out.println("Social Security Number not entered");
+            System.exit(0);
+        }
+        this.ssn = ssn;
+    }
+
+    // Assume this must be performed first
+    public void meetWithHrForBenefitAndSalryInfo() {
+        metWithHr = true;
+    }
+
+    // Assume this is must be performed second
+    public void meetDepartmentStaff() {
+        if(metWithHr) {
+            metDeptStaff = true;
+        } else {
+            System.out.println("Sorry, you cannot meet with "
+                    + "department staff until you have met with HR.");
+        }
+    }
+
+    // Assume this must be performed third
+    public void reviewDeptPolicies() {
+        if(metWithHr && metDeptStaff) {
+            reviewedDeptPolicies = true;
+        } else {
+            System.out.println("Sorry, you cannot review "
+                    + " department policies until you have first met with HR "
+                    + "and then with department staff.");
+        }
+    }
+
+    // Assume this must be performed 4th
+    public void moveIntoCubicle(String cubeId) {
+        if(metWithHr && metDeptStaff && reviewedDeptPolicies) {
+            this.cubeId = cubeId;
+            this.movedIn = true;
+        } else {
+            System.out.println("Sorry, you cannot move in to a "
+                    + "cubicle until you have first met with HR "
+                    + "and then with department staff, and then reviewed"
+                    + "department policies.");
+        }
+
+    }
+
+    public String getStatus() {
+        if(metWithHr && metDeptStaff
+           && reviewedDeptPolicies && movedIn) {
+            return "Orientation is complete";
+        } else {
+            return "Orientation in progress...";
+        }
+    }
+    
+}
---
+++ src/encapsulationlab/EncapsulationLab.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package encapsulationlab;
+
+/**
+ *
+ * @author eennis
+ */
+public class EncapsulationLab {
+
+    /*
+            Are we violating SRP (Single Responsibility Principle)?
+        
+            HINT: think about what we are trying to do ... hire an employee.
+            Whose job is it to do this? The current class? Or, a new class?
+            Should we do the work here or delegate to the new class? You
+            may need to create a new class that talks to Employee. This does
+            not have to be a GUI class.
+        */
+    
+    public static void main(String[] args){
+        
+        Employee employee = new Employee();
+        
+        employee.createEmployee("Peter", "Piper", "333-1234");
+        
+        employee.meetWithHrForBenefitAndSalryInfo();
+        employee.meetDepartmentStaff();
+        employee.reviewDeptPolicies();
+        employee.moveIntoCubicle("1234");
+
+        System.out.println("The employee's status is: " + employee.getStatus());
+    }
+    
+}
